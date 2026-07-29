61f45eb9b63bf238b857b11e8a6882c9f6f9dde3
wutcha <kevin.liu0901@gmail.com>
2026-07-29 18:43:55 -0400

    lines

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+model_path = 'models/gesture_recognizer.task'
+
 JUTSU_SEQUENCES = {
     "fireball": ["snake","ram","monkey","boar","horse","tiger"],
     "chidori": ["ox","rabbit","monkey","chidori_finisher"]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mediapipe as mp
 import cv2 as cv
 import time
+from config import model_path
 
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
@@ -14,7 +15,7 @@ def result_callback(result, output_image, timestamp_ms):
     latest_result = result
 
 # 7 default handsigns
-basic_model_path = 'models/gesture_recognizer.task'
+basic_model_path = model_path
 
 # 12 jutsu handsigns
 # add here later
@@ -25,6 +26,39 @@ options = GestureRecognizerOptions(
     result_callback=result_callback,
     num_hands = 2
 )
+
+def draw_hand(frame, latest_result):
+    HAND_CONNECTIONS = [
+    (0, 1), (1, 2), (2, 3), (3, 4),           # Thumb
+    (0, 5), (5, 6), (6, 7), (7, 8),           # Index
+    (0, 9), (9, 10), (10, 11), (11, 12),      # Middle
+    (0, 13), (13, 14), (14, 15), (15, 16), # Ring
+    (0, 17), (17, 18), (18, 19), (19, 20),  # Pinky
+    (5, 9), (9, 13), (13, 17)                 # Palm
+    ]
+
+    latest_landmarks = latest_result.hand_landmarks
+    #print(latest_landmarks)
+    for landmarks in latest_landmarks:
+
+        #lines
+        for idx1, idx2 in HAND_CONNECTIONS:
+            if idx1 < len(landmarks) and idx2 < len(landmarks):
+                pt1 = landmarks[idx1]
+                pt2 = landmarks[idx2]
+                shifted_pt1 = (int(pt1.x * frame.shape[1]), int(pt1.y * frame.shape[0]))
+                shifted_pt2 = (int(pt2.x * frame.shape[1]), int(pt2.y * frame.shape[0]))     
+                cv.line(frame, shifted_pt1, shifted_pt2, (0,0,0), 2)
+
+        #landmarks
+        for landmark in landmarks:
+            x = int(landmark.x * frame.shape[1])
+            y = int(landmark.y * frame.shape[0])
+            cv.circle(frame, (x,y), 5, (0,255,0), -1)
+    
+    
+
+
 
 with GestureRecognizer.create_from_options(options) as recognizer:
     capture = cv.VideoCapture(0)
@@ -39,16 +73,21 @@ with GestureRecognizer.create_from_options(options) as recognizer:
 
         # drawing stuff
         if latest_result and latest_result.hand_landmarks:
-            latest_landmarks = latest_result.hand_landmarks
-            for landmarks in latest_landmarks:
-                for landmark in landmarks:
-                    x = int(landmark.x * frame.shape[1])
-                    y = int(landmark.y * frame.shape[0])
-                    cv.circle(frame, (x,y), 5, (0,255,0), -1)
+            draw_hand(frame, latest_result)
+
+            # latest_landmarks = latest_result.hand_landmarks
+            # for landmarks in latest_landmarks:
+            #     for landmark in landmarks:
+            #         x = int(landmark.x * frame.shape[1])
+            #         y = int(landmark.y * frame.shape[0])
+            #         cv.circle(frame, (x,y), 5, (0,255,0), -1)
             
             if latest_result.gestures:
+                #print(latest_result,'\n')
                 gesture = latest_result.gestures[0][0].category_name
-                cv.putText(frame,gesture, (50,50), cv.FONT_HERSHEY_TRIPLEX, 1, (0,0,0), 2)
+                score = str(round(latest_result.gestures[0][0].score,2))
+                cv.putText(frame,gesture,(50,50), cv.FONT_HERSHEY_TRIPLEX, 1, (0,0,0), 2)
+                cv.putText(frame,score,(50,75), cv.FONT_HERSHEY_TRIPLEX,1, (0,0,0), 2)
 
         cv.imshow("frame", frame)
 
